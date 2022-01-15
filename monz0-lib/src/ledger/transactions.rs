ffec4397a5b0ad9440bbfb63b0093ae6271adb08
Daniel Eades <danieleades@hotmail.com>
2022-01-15 12:27:44 +0000

    add tests to 'ledger'

--- monz0-lib/src/ledger/transactions.rs
+++ monz0-lib/src/ledger/transactions.rs
@@ -13,14 +13,16 @@ pub struct Transactions<'a> {
 
 impl<'a> Transactions<'a> {
     /// Add a transaction to the ledger
+    ///
+    /// a
     pub fn push(&mut self, transaction: (&'a Pot, i64)) {
         let (pot, amount) = transaction;
 
         #[allow(clippy::cast_possible_truncation, clippy::cast_sign_loss)]
-        if amount < 0 {
-            self.withdrawals.push((pot, amount.abs() as u32));
-        } else {
-            self.deposits.push((pot, amount as u32));
+        match Ord::cmp(&amount, &0) {
+            std::cmp::Ordering::Less => self.withdrawals.push((pot, amount.abs() as u32)),
+            std::cmp::Ordering::Equal => (),
+            std::cmp::Ordering::Greater => self.deposits.push((pot, amount as u32)),
         }
     }
 
@@ -28,6 +30,11 @@ impl<'a> Transactions<'a> {
     #[must_use]
     pub fn is_empty(&self) -> bool {
         self.withdrawals.is_empty() && self.deposits.is_empty()
+    }
+
+    /// Returns the total number of transactions
+    pub fn len(&self) -> usize {
+        self.withdrawals.len() + self.deposits.len()
     }
 }
 
@@ -48,5 +55,56 @@ impl<'a> IntoIterator for &'a Transactions<'a> {
             .map(|(pot, deposit)| (*pot, i64::from(*deposit)));
 
         withdrawals.chain(deposits)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use monzo::Pot;
+
+    use super::Transactions;
+
+    fn dummy_pot() -> Pot {
+        let pot = r#"
+        {
+            "id": "pot_1234",
+            "name": "Savings",
+            "style": "teal",
+            "balance": 10,
+            "currency": "GBP",
+            "goal_amount": 1000000,
+            "type": "flexible_savings",
+            "product_id": "XXX",
+            "current_account_id": "acc_1234",
+            "cover_image_url": "",
+            "isa_wrapper": "ISA",
+            "round_up": false,
+            "round_up_multiplier": null,
+            "is_tax_pot": false,
+            "created": "2019-04-28T06:36:54.318Z",
+            "updated": "2019-05-11T00:31:04.256Z",
+            "deleted": false,
+            "locked": false,
+            "charity_id": "",
+            "available_for_bills": false
+        }
+        "#;
+
+        serde_yaml::from_str(pot).unwrap()
+    }
+
+    #[test]
+    fn insert() {
+        let mut transactions = Transactions::default();
+        let pot = dummy_pot();
+
+        transactions.push((&pot, 100));
+        assert!(transactions.deposits.len() == 1);
+
+        transactions.push((&pot, -100));
+        assert!(transactions.withdrawals.len() == 1);
+
+        transactions.push((&pot, 0));
+        assert!(transactions.len() == 2);
     }
 }
