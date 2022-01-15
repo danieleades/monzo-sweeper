61e774e86c59aa4a7c95bcf4774601bd52ab887b
Daniel Eades <danieleades@hotmail.com>
2022-01-15 11:12:30 +0000

    refactor

--- src/app/show.rs
+++ src/app/show.rs
@@ -1,15 +1,9 @@
-use clap::Parser;
 use confy::ConfyError;
 
 use crate::config;
 
-#[derive(Debug, Default, Parser, Clone, Copy)]
-pub struct Show;
-
-impl Show {
-    pub fn run() -> Result<(), ConfyError> {
-        let operations = config::operations()?;
-        println!("{:#?}", operations);
-        Ok(())
-    }
+pub fn run() -> Result<(), ConfyError> {
+    let operations = config::operations()?;
+    println!("{:#?}", operations);
+    Ok(())
 }
